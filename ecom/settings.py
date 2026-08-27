f8d1dcb5eab7e84b7538b2156a1492d7a07f2f22
Gaurav Malankar <malankargaurav4@gmail.com>
2026-08-27 21:09:25 +0530

    Third commit

--- ecom/settings.py
+++ ecom/settings.py
@@ -93,11 +93,10 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
-
 }
 
 
